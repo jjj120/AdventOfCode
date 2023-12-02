78014600031f1f4fa50d079b49ee28e31dd5a4a5
Jonas Mueller <jonas.mueller@gmx.at>
2023-12-02 12:19:59 +0100

    Add template for faster line by line handling of files

---
+++ template/tmpl.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func handleLine(line string) int {
+	fmt.Print(line)
+	return 0
+}
+
+func main() {
+	// Open the file
+	file, err := os.Open("2.in")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	var sum = 0
+	// Iterate through each line
+	for scanner.Scan() {
+		line := scanner.Text()
+		sum += handleLine(line)
+	}
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
