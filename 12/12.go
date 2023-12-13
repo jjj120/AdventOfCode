f3df0ccae7d9b71199c04e4d7f9f7ceb3c15ada0
Jonas Mueller <jonas.mueller@gmx.at>
2023-12-13 10:56:04 +0100

    Add Dec12 ex2

--- 12/12.go
+++ 12/12.go
@@ -15,88 +15,144 @@ func check(e error) {
 	}
 }
 
-// returns 0 if line is correct, 1 if line is incorrect, 2 if line has ? in it
-func checkLine(line string) int {
-	stringParts := strings.Split(line, " ")
-	data := stringParts[0]
+const UNUSED = -2
+const PRINT = false
+const ENLARGEMENT = 5
 
-	for _, char := range data {
-		if char == '?' {
-			return 2
+func canPlace(springs string, startIndex int, groupSize int) bool {
+	for i := startIndex; i < startIndex+groupSize; i++ {
+		if i >= len(springs) || springs[i] == '.' {
+			return false
 		}
 	}
 
-	checkSums := stringParts[1]
-
-	regDam := regexp.MustCompile("[#]+")
-	regNum := regexp.MustCompile("[0-9]+")
-
-	damagedSprings := regDam.FindAllString(data, -1)
-	damagedSpringsNumbers := regNum.FindAllString(checkSums, -1)
-
-	if len(damagedSprings) != len(damagedSpringsNumbers) {
-		return 1
+	if startIndex > 0 && springs[startIndex-1] == '#' {
+		return false
 	}
 
-	for i := 0; i < len(damagedSprings); i++ {
-		num, err := strconv.ParseInt(damagedSpringsNumbers[i], 10, 0)
-		check(err)
-
-		if len(damagedSprings[i]) != int(num) {
-			return 1
-		}
+	if startIndex+groupSize < len(springs) && springs[startIndex+groupSize] == '#' {
+		return false
 	}
-	return 0
+
+	return true
+
+	// if len(springs)-startIndex < groupSize {
+	// 	// not enough space anymore
+	// 	// fmt.Println("Exit because not enough space")
+	// 	return false
+	// }
+
+	// if startIndex > 0 && springs[startIndex-1] == '#' {
+	// 	return false
+	// }
+
+	// if !(startIndex <= len(springs)-groupSize) {
+	// 	return false
+	// }
+
+	// for i := 0; i < groupSize; i++ {
+	// 	if springs[startIndex+i] == '.' {
+	// 		return false
+	// 	}
+	// }
+
+	// //foundSpace = foundSpace && (startIndex+groupSize == len(springs) || (springs[startIndex+groupSize] != '#')) // check if string has an end where it should have
+	// return (startIndex == len(springs)-groupSize) || (springs[startIndex+groupSize] != '#') // check if string has an end where it should have
+
 }
 
-// returns sum if correct and -1 if incorrect
-func countLinePoss(line string, startIndex int, channel chan int) {
-
-	check := checkLine(line)
-	if check == 0 {
-		channel <- 1
-		return
+func countPoss(memory *[][]int, springs string, groups []int, startIndex int, nextGroupInd int) int {
+	if nextGroupInd >= len(groups) {
+		for i := startIndex; i < len(springs); i++ {
+			if springs[i] == '#' {
+				return 0
+			}
+		}
+		return 1
 	}
-	if check == 1 {
-		channel <- (-1)
-		return
-	}
-
-	sum := 0
-	for i := startIndex; i < len(line); i++ {
-		if line[i] != '?' {
-			continue
-		}
-
-		poss1 := line[:i] + "." + line[i+1:]
-		poss2 := line[:i] + "#" + line[i+1:]
-
-		chan1 := make(chan int)
-		chan2 := make(chan int)
-
-		go countLinePoss(poss1, i, chan1)
-		go countLinePoss(poss2, i, chan2)
-
-		res1 := <-chan1
-		if res1 > 0 {
-			// fmt.Printf("Got %d from chan1\n", res1)
-			sum += res1
-		}
-
-		res2 := <-chan2
-		if res2 > 0 {
-			// fmt.Printf("Got %d from chan2\n", res2)
-			sum += res2
-		}
+	if startIndex >= len(springs) {
+		return 0
 	}
 
-	channel <- sum
+	if (*memory)[startIndex][nextGroupInd] != UNUSED {
+		// fmt.Println("Already Set data")
+		return (*memory)[startIndex][nextGroupInd]
+	}
+
+	currPoss := 0
+
+	if canPlace(springs, startIndex, groups[nextGroupInd]) {
+
+		if PRINT {
+			fmt.Printf("%d, %d: \t", nextGroupInd, groups[nextGroupInd])
+			for i, char := range springs {
+				if i >= startIndex && i < startIndex+groups[nextGroupInd] {
+					fmt.Printf("\033[1m\033[36m%c\033[0m\033[0m", char)
+				} else {
+					fmt.Printf("%c", char)
+				}
+			}
+			fmt.Println("")
+		}
+		newStart := startIndex + groups[nextGroupInd] + 1
+		currPoss += countPoss(memory, springs, groups, newStart, nextGroupInd+1)
+	}
+
+	if !(startIndex > 0 && springs[startIndex-1] == '#') {
+		currPoss += countPoss(memory, springs, groups, startIndex+1, nextGroupInd)
+	}
+
+	(*memory)[startIndex][nextGroupInd] = currPoss
+	return currPoss
+}
+
+func splitData(line string) (string, []int) {
+	stringParts := strings.Split(line, " ")
+
+	regNum := regexp.MustCompile("[0-9]+")
+
+	damagedSpringsNumbers := regNum.FindAllString(stringParts[1], -1)
+
+	var numbers []int
+	for i := 0; i < len(damagedSpringsNumbers); i++ {
+		num, err := strconv.ParseInt(damagedSpringsNumbers[i], 10, 0)
+		check(err)
+		numbers = append(numbers, int(num))
+	}
+	return stringParts[0], numbers
 }
 
 func handleLine(line string) int {
-	channel := make(chan int)
-	go countLinePoss(line, 0, channel)
-	return <-channel
+	if ENLARGEMENT != 1 {
+		line = (strings.Repeat(strings.Split(line, " ")[0]+"?", ENLARGEMENT))[:len(strings.Split(line, " ")[0])*ENLARGEMENT+ENLARGEMENT-1] + " " + strings.TrimRight(strings.Repeat(strings.Split(line, " ")[1]+",", ENLARGEMENT), ",")
+	}
+
+	data, groups := splitData(line)
+
+	var memory [][]int
+	for i := 0; i < len(data); i++ {
+		memory = append(memory, make([]int, len(groups)))
+		for j := 0; j < len(groups); j++ {
+			(memory)[i][j] = UNUSED
+		}
+	}
+
+	res := countPoss(&memory, data, groups, 0, 0)
+
+	if PRINT {
+		for i, lineMem := range memory {
+			fmt.Printf("%d: ", i)
+			fmt.Println(lineMem)
+		}
+
+		fmt.Println("")
+
+		fmt.Println(line)
+		fmt.Printf("found solution %d\n\n", res)
+		fmt.Println(": ", res)
+	}
+
+	return res
 }
 
 func main() {
@@ -117,8 +173,8 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		fmt.Printf("\r\r\r\r\r%d", i)
-		i++
 		sum += handleLine(line)
+		i++
 	}
 	fmt.Print("\n")
 
