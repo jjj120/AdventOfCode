155479ac45b07d73862def8a63f699f95698d2d8
Jonas Mueller <jonas.mueller@gmx.at>
2023-12-18 08:46:21 +0100

    Add Dec18 ex2

--- 18/18.go
+++ 18/18.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -15,9 +16,8 @@ func check(e error) {
 }
 
 type digInstruction struct {
-	dir   int
-	len   int
-	color int
+	dir int
+	len int
 }
 
 const (
@@ -29,120 +29,64 @@ const (
 
 func parseInstruction(line string) digInstruction {
 	splitString := strings.Split(line, " ")
-	var instr digInstruction
-	switch line[0] {
-	case 'U':
-		instr.dir = UP
-	case 'R':
-		instr.dir = RIGHT
-	case 'D':
-		instr.dir = DOWN
-	case 'L':
-		instr.dir = LEFT
-	}
-
-	num, err := strconv.ParseInt(splitString[1], 10, 0)
-	check(err)
-	instr.len = int(num)
-
 	splitString[2] = strings.ReplaceAll(splitString[2], "(", "")
 	splitString[2] = strings.ReplaceAll(splitString[2], ")", "")
 	splitString[2] = strings.ReplaceAll(splitString[2], "#", "")
 
-	num, err = strconv.ParseInt(splitString[2], 16, 0)
-	check(err)
+	var instr digInstruction
+	switch splitString[2][5] - '0' {
+	case 0:
+		instr.dir = RIGHT
+	case 1:
+		instr.dir = DOWN
+	case 2:
+		instr.dir = LEFT
+	case 3:
+		instr.dir = UP
+	}
 
-	instr.color = int(num)
+	num, err := strconv.ParseInt(splitString[2][0:5], 16, 0)
+	check(err)
+	instr.len = int(num)
 
 	return instr
 }
 
-func handleLine(digPlan [][]bool, line string, currX, currY *int) {
-	inst := parseInstruction(line)
+func addToVertices(line string, vertices *[][2]int, x, y *int) {
+	instr := parseInstruction(line)
 
-	for i := 0; i < inst.len; i++ {
-		switch inst.dir {
-		case UP:
-			*currY--
-		case RIGHT:
-			*currX++
-		case DOWN:
-			*currY++
-		case LEFT:
-			*currX--
-		}
-		if *currX < 0 || *currY < 0 || *currX >= len(digPlan) || *currY >= len(digPlan) {
-			fmt.Printf("Out of bounds: %d, %d\n", *currX, *currY)
-			os.Exit(1)
-		}
-		fmt.Printf("X: %d, Y: %d\n", *currX, *currY)
-		digPlan[*currY][*currX] = true
+	switch instr.dir {
+	case UP:
+		*y -= instr.len
+	case RIGHT:
+		*x += instr.len
+	case DOWN:
+		*y += instr.len
+	case LEFT:
+		*x -= instr.len
 	}
+
+	p2 := [2]int{*x, *y}
+	*vertices = append(*vertices, p2)
 }
 
-func printDigPlan(digPlan [][]bool) {
-	for _, row := range digPlan {
-		for _, col := range row {
-			if col {
-				fmt.Print("#")
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
+func calcArea(vertices [][2]int) int {
+	if len(vertices)%2 != 0 {
+		vertices = append(vertices, vertices[0])
 	}
+	var area int
+	for i := 0; i < len(vertices); i += 2 {
+		area += vertices[i][0]*vertices[i+1][1] - vertices[i+1][0]*vertices[i][1]
+	}
+	return area
 }
 
-func floodFill(digPlan [][]bool, x, y int) {
-	if x < 0 || y < 0 || x >= len(digPlan) || y >= len(digPlan) {
-		return
+func calcPerimeter(vertices [][2]int) int {
+	var perimeter int
+	for i := 0; i < len(vertices)-1; i++ {
+		perimeter += int(math.Sqrt(math.Pow(float64(vertices[i][0]-vertices[i+1][0]), 2) + math.Pow(float64(vertices[i][1]-vertices[i+1][1]), 2)))
 	}
-
-	if !digPlan[y][x] {
-		digPlan[y][x] = true
-		floodFill(digPlan, x-1, y)
-		floodFill(digPlan, x+1, y)
-		floodFill(digPlan, x, y-1)
-		floodFill(digPlan, x, y+1)
-		floodFill(digPlan, x-1, y-1)
-		floodFill(digPlan, x+1, y-1)
-		floodFill(digPlan, x-1, y+1)
-		floodFill(digPlan, x+1, y+1)
-	}
-}
-
-func fillOutsideOfLine(digPlan [][]bool) {
-	floodFill(digPlan, 0, 0)
-}
-
-func countIncluded(digPlan [][]bool) int {
-	count := 0
-
-	for _, row := range digPlan {
-		for _, col := range row {
-			if col {
-				count++
-			}
-		}
-	}
-
-	return count
-}
-
-func initDigPlan(size int) [][]bool {
-	digPlan := make([][]bool, size)
-	for i := 0; i < size; i++ {
-		digPlan[i] = make([]bool, size)
-	}
-	return digPlan
-}
-
-func invertDigPlan(digPlan [][]bool) {
-	for i, row := range digPlan {
-		for j, col := range row {
-			digPlan[i][j] = !col
-		}
-	}
+	return perimeter
 }
 
 func main() {
@@ -158,25 +102,21 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	var sum = 0
-	size := 1000
-	digPlan := initDigPlan(size)
-	currX, currY := size/2, size/2
+	currX, currY := 0, 0
 	// Iterate through each line
+
+	vertices := make([][2]int, 0)
+	vertices = append(vertices, [2]int{0, 0})
 	for scanner.Scan() {
 		line := scanner.Text()
-		handleLine(digPlan, line, &currX, &currY)
+		addToVertices(line, &vertices, &currX, &currY)
 	}
 
-	// printDigPlan(digPlan)
-	fmt.Println()
-	sum = countIncluded(digPlan)
+	sum = calcArea(vertices)           // area of polygon
+	sum += calcPerimeter(vertices) / 2 // add half of perimeter for the fields on the side, that the edge runs through in the middle
+	sum++                              // add 1 for the one field outside the perimeter
 
-	fillOutsideOfLine(digPlan)
-	invertDigPlan(digPlan)
-
-	sum += countIncluded(digPlan)
-
-	// printDigPlan(digPlan)
+	fmt.Printf("Area: %d, Perimeter: %d\n", calcArea(vertices), calcPerimeter(vertices))
 
 	// Check for errors during scanning
 	if err := scanner.Err(); err != nil {
