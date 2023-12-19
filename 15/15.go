2f231f5a1c48ef182cc391edf69bc085e1dbf3b1
Jonas Mueller <jonas.mueller@gmx.at>
2023-12-19 20:03:35 +0100

    Add Dec15 ex1

---
+++ 15/15.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Ingredient struct {
+	name       string
+	capacity   int
+	durability int
+	flavor     int
+	texture    int
+	calories   int
+}
+
+func parseIngredient(line string) Ingredient {
+	var ing Ingredient
+	// n, err := fmt.Sscanf(line, "%s: capacity %d, durability %d, flavor %d, texture %d, calories %d", &ing.name, &ing.capacity, &ing.durability, &ing.flavor, &ing.texture, &ing.calories)
+
+	str1 := strings.Split(line, ": ")
+	ing.name = str1[0]
+
+	str2 := strings.Split(str1[1], ", ")
+	for _, str := range str2 {
+		str3 := strings.Split(str, " ")
+
+		var err error
+		switch str3[0] {
+		case "capacity":
+			ing.capacity, err = strconv.Atoi(str3[1])
+			check(err)
+		case "durability":
+			ing.durability, err = strconv.Atoi(str3[1])
+			check(err)
+		case "flavor":
+			ing.flavor, err = strconv.Atoi(str3[1])
+			check(err)
+		case "texture":
+			ing.texture, err = strconv.Atoi(str3[1])
+			check(err)
+		case "calories":
+			ing.calories, err = strconv.Atoi(str3[1])
+			check(err)
+		}
+	}
+
+	return ing
+}
+
+func calcScore(ingredients []Ingredient, amount []int) int {
+	sum := 0
+	for _, am := range amount {
+		sum += am
+	}
+
+	if sum != 100 {
+		return 0
+	}
+
+	cap := 0
+	dur := 0
+	fla := 0
+	tex := 0
+
+	for i, ing := range ingredients {
+		cap += ing.capacity * amount[i]
+		dur += ing.durability * amount[i]
+		fla += ing.flavor * amount[i]
+		tex += ing.texture * amount[i]
+	}
+
+	if cap < 0 || dur < 0 || fla < 0 || tex < 0 {
+		return 0
+	}
+
+	return cap * dur * fla * tex
+}
+
+func findMaxScore(ingredients []Ingredient, amounts []int) int {
+	if len(amounts) == len(ingredients) {
+		// fmt.Printf("Finished with amounts len: %d and Ingredients len: %d\n", len(amounts), len(ingredients))
+		// if calcScore(ingredients, amounts) != 0 && calcScore(ingredients, amounts) > 0 {
+		// 	fmt.Println(calcScore(ingredients, amounts))
+		// }
+		return calcScore(ingredients, amounts)
+	}
+
+	maxAmount := 100
+	if len(amounts) > 0 {
+		for _, am := range amounts {
+			maxAmount -= am
+		}
+	}
+
+	maxScore := 0
+	for i := 0; i <= maxAmount; i++ {
+		newAmounts := make([]int, len(amounts))
+		copy(newAmounts, amounts)
+		newAmounts = append(newAmounts, i)
+
+		// fmt.Println(newAmounts)
+
+		score := findMaxScore(ingredients, newAmounts)
+		maxScore = max(maxScore, score)
+	}
+
+	return maxScore
+}
+
+func handleLine(line string) Ingredient {
+	return parseIngredient(line)
+}
+
+func main() {
+	// Open the file
+	file, err := os.Open("15.in")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	var sum = 0
+	ingredients := make([]Ingredient, 0)
+	// Iterate through each line
+	for scanner.Scan() {
+		line := scanner.Text()
+		ingredients = append(ingredients, handleLine(line))
+	}
+
+	sum = findMaxScore(ingredients, make([]int, 0))
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
---
+++ 15/go.mod
@@ -0,0 +1,3 @@
+module mueller.at/adventofcode/15/m/v2
+
+go 1.21.4
