0e240b50a4a7b12dd45dde06948082386b17f9f6
Jonas Mueller <jonas.mueller@gmx.at>
2023-12-19 14:03:34 +0100

    Add Dec19 ex2

--- 19/19.go
+++ 19/19.go
@@ -26,6 +26,14 @@ type Rule struct {
 	end     bool
 }
 
+const MAX_VALUE = 4000
+
+const (
+	ACCEPT = iota
+	REJECT
+	CONTINUE
+)
+
 func parseRule(line string, ruleMap map[string][]Rule) {
 	line = strings.ReplaceAll(line, "}", "")
 	split1 := strings.Split(line, "{")
@@ -93,12 +101,6 @@ func parsePart(line string) Part {
 	return p
 }
 
-const (
-	ACCEPT = iota
-	REJECT
-	CONTINUE
-)
-
 func nextRule(currRule []Rule, part Part, ruleMap map[string][]Rule) ([]Rule, int) {
 	if len(currRule) == 0 {
 		return currRule, REJECT
@@ -165,10 +167,8 @@ func accept(part Part, ruleMap map[string][]Rule) bool {
 
 		currRule, ok = nextRule(currRule, part, ruleMap)
 		if ok == ACCEPT {
-			fmt.Printf("ACCEPT: %v\n", part)
 			return true
 		} else if ok == REJECT {
-			fmt.Printf("REJECT: %v\n", part)
 			return false
 		}
 	}
@@ -178,14 +178,91 @@ func calcWorth(part Part) int {
 	return part.x + part.m + part.a + part.s
 }
 
-func calcAccepted(parts []Part, ruleMap map[string][]Rule) int {
-	sum := 0
-	for _, part := range parts {
-		if accept(part, ruleMap) {
-			sum += calcWorth(part)
+func calcAcceptedBruteForce(ruleMap map[string][]Rule) int {
+	fmt.Printf("     %d\n", MAX_VALUE*MAX_VALUE*MAX_VALUE*MAX_VALUE)
+	counter := 0
+	accepted := 0
+	for x := 0; x <= MAX_VALUE; x++ {
+		for m := 0; m <= MAX_VALUE; m++ {
+			for a := 0; a <= MAX_VALUE; a++ {
+				for s := 0; s <= MAX_VALUE; s++ {
+					var p Part
+					p.x = x
+					p.m = m
+					p.a = a
+					p.s = s
+					if accept(p, ruleMap) {
+						accepted++
+					}
+					counter++
+					if counter%1000000 == 0 {
+						fmt.Printf("\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\r\rPart %d, acc: %d", counter, accepted)
+					}
+				}
+			}
 		}
 	}
-	return sum
+
+	return counter
+}
+
+func copyLimits(limits map[string][2]int) map[string][2]int {
+	// copy limits map
+	newLimits := make(map[string][2]int)
+	for k, v := range limits {
+		newLimits[k] = v
+	}
+	return newLimits
+}
+
+func countAccepted(ruleMap map[string][]Rule, currRuleName string, limits map[string][2]int) int {
+	currNextRules := ruleMap[currRuleName]
+	accepted := 0
+
+	for _, rule := range currNextRules {
+		if rule.end {
+			if rule.a == "A" {
+				toReturn := 1
+				for _, limit := range limits {
+					toReturn *= limit[1] - limit[0] + 1
+				}
+				accepted += toReturn
+			} else if rule.a == "R" {
+				accepted += 0
+			} else {
+				accepted += countAccepted(ruleMap, rule.a, limits)
+			}
+			break
+		}
+
+		nextFromAccepting := limits[rule.a][0]
+		nextToAccepting := limits[rule.a][1]
+
+		if rule.smaller {
+			nextToAccepting = rule.num - 1
+		} else {
+			nextFromAccepting = rule.num + 1
+		}
+
+		if nextFromAccepting > nextToAccepting {
+			// no possible values, limits dont change
+			continue
+		}
+
+		newLimits := copyLimits(limits)
+
+		newLimits[rule.a] = [2]int{nextFromAccepting, nextToAccepting}
+
+		accepted += countAccepted(ruleMap, rule.next, newLimits)
+
+		if rule.smaller {
+			limits[rule.a] = [2]int{rule.num, limits[rule.a][1]}
+		} else {
+			limits[rule.a] = [2]int{limits[rule.a][0], rule.num}
+		}
+	}
+
+	return accepted
 }
 
 func main() {
@@ -213,7 +290,6 @@ func main() {
 	rej.end = true
 	ruleMap["R"] = append(ruleMap["R"], rej)
 
-	var parts []Part
 	rules := true
 
 	// Iterate through each line
@@ -226,20 +302,23 @@ func main() {
 
 		if rules {
 			parseRule(line, ruleMap)
-		} else {
-			parts = append(parts, parsePart(line))
 		}
 	}
 
-	fmt.Println(ruleMap)
-	fmt.Println(parts)
+	limits := make(map[string][2]int)
+	limits["x"] = [2]int{1, MAX_VALUE}
+	limits["m"] = [2]int{1, MAX_VALUE}
+	limits["a"] = [2]int{1, MAX_VALUE}
+	limits["s"] = [2]int{1, MAX_VALUE}
 
-	sum = calcAccepted(parts, ruleMap)
+	sum = countAccepted(ruleMap, "in", limits)
 
 	// Check for errors during scanning
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
 	}
 
+	fmt.Printf("     %d\n", MAX_VALUE*MAX_VALUE*MAX_VALUE*MAX_VALUE)
+	fmt.Println("R:   167409079868000")
 	fmt.Printf("Sum: %d\n", sum)
 }
