0e4b2054301afae3a33216d55054ff684f5f16bd
Jonas Mueller <jonas.mueller@gmx.at>
2024-12-14 09:48:49 +0100

    Add Dec14 ex2

--- 14/14.go
+++ 14/14.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func check(e error) {
@@ -39,12 +40,16 @@ func parseRobot(line string) Robot {
 }
 
 func simRobots(robots []Robot, t int) {
+	absT := t
+	if t < 0 {
+		absT = -t
+	}
 	for i := range robots {
 		robots[i].px += robots[i].vx * t
 		robots[i].py += robots[i].vy * t
 
-		robots[i].px = (robots[i].px + t*WIDTH) % WIDTH   // get the robots back into the field
-		robots[i].py = (robots[i].py + t*HEIGHT) % HEIGHT // get the robots back into the field
+		robots[i].px = (robots[i].px + absT*WIDTH) % WIDTH   // get the robots back into the field
+		robots[i].py = (robots[i].py + absT*HEIGHT) % HEIGHT // get the robots back into the field
 
 		assert(robots[i].px >= 0 && robots[i].px < WIDTH, "robot out of bounds")
 		assert(robots[i].py >= 0 && robots[i].py < HEIGHT, "robot out of bounds")
@@ -92,9 +97,10 @@ func printRobotsField(robots []Robot) {
 	for y := 0; y < HEIGHT; y++ {
 		for x := 0; x < WIDTH; x++ {
 			if positionCount[Coord{x, y}] > 0 {
-				fmt.Printf("%d", positionCount[Coord{x, y}])
+				// fmt.Printf("%d", positionCount[Coord{x, y}])
+				fmt.Printf("█")
 			} else {
-				fmt.Printf(".")
+				fmt.Printf(" ")
 			}
 		}
 		fmt.Println()
@@ -106,6 +112,75 @@ func printRobots(robots []Robot) {
 	for _, r := range robots {
 		fmt.Printf("p=(%d, %d) v=(%d, %d)\n", r.px, r.py, r.vx, r.vy)
 	}
+}
+
+func printSimulation(robots []Robot) {
+	buf := bufio.NewReader(os.Stdin)
+	time := 0
+	for {
+		printRobotsField(robots)
+		fmt.Printf("Time: %d, Compactness: %d\n", time, calcCompactness(robots))
+		str, err := buf.ReadString('\n')
+		check(err)
+		if str[0] == 'q' {
+			break
+		} else if str[0] == 'p' {
+			time--
+			simRobots(robots, -1)
+			continue
+		}
+		// convert to number
+		num, err := strconv.Atoi(str[:len(str)-1 /* remove newline */])
+		if err == nil {
+			fmt.Printf("Simulating %d seconds to %d\n", num-time, num)
+			simRobots(robots, num-time)
+			time = num
+			continue
+		}
+		simRobots(robots, 1)
+		time++
+	}
+	time--
+	fmt.Printf("Simulation ended at time: %d\n", time)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func findMinCompactness(robots []Robot) (int, int) {
+	robotsCopy := make([]Robot, len(robots))
+	copy(robotsCopy, robots)
+
+	simTime := 10000
+	minCompactness := calcCompactness(robotsCopy)
+	minTime := 0
+	for t := 1; t < simTime; t++ {
+		fmt.Printf("Simulating %d seconds\r", t)
+		simRobots(robotsCopy, 1)
+		compactness := calcCompactness(robotsCopy)
+		if compactness < minCompactness {
+			minCompactness = compactness
+			minTime = t
+		}
+	}
+	fmt.Print("                                                               \r")
+	return minTime, minCompactness
+}
+
+func calcCompactness(robots []Robot) int {
+	// calculate the compactness of the robots
+	// compactness = sum of distances between all robots
+	compactness := 0
+	for i := 0; i < len(robots); i++ {
+		for j := i + 1; j < len(robots); j++ {
+			compactness += abs(robots[i].px-robots[j].px) + abs(robots[i].py-robots[j].py)
+		}
+	}
+	return compactness
 }
 
 func main() {
@@ -127,14 +202,26 @@ func main() {
 		robots = append(robots, parseRobot(line))
 	}
 
-	printRobots(robots)
-	fmt.Println("")
-	printRobotsField(robots)
-	simRobots(robots, SIM_TIME)
+	// printRobots(robots)
+	// fmt.Println("")
+	// printRobotsField(robots)
+	// simRobots(robots, SIM_TIME)
+	// printRobotsField(robots)
+	minTime, minCompactness := findMinCompactness(robots)
+	fmt.Printf("Min time: %d, Min compactness: %d\n", minTime, minCompactness)
+
+	// printSimulation(robots)
+	// interesting times:
+	// 51, 86 -> 35, 68 to next
+	// 154, 187 -> 33, 70 to next
+	// 257, 288 -> 31, 72 to next
+	// 360, 389 -> 29, ...
+
+	simRobots(robots, minTime)
 	printRobotsField(robots)
 
-	var sum = 0
-	sum = countScore(robots)
+	var sum = minTime
+	// sum = countScore(robots)
 
 	// Check for errors during scanning
 	if err := scanner.Err(); err != nil {
