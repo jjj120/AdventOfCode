73eb8e1fe472fadba1ba04f29a190f889cc1c39a
Jonas Mueller <jonas.mueller@gmx.at>
2024-12-20 10:14:19 +0100

    Add Dec20 ex1

---
+++ 20/20.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+)
+
+func assert(condition bool, message string) {
+	if !condition {
+		panic(message)
+	}
+}
+
+func assertf(condition bool, message string, args ...interface{}) {
+	if !condition {
+		panic(fmt.Sprintf(message, args...))
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+type Coord struct {
+	x, y int
+}
+
+func getStartEnd(labyrinth []string) (startpoint Coord, endpoint Coord) {
+	for y, line := range labyrinth {
+		for x, char := range line {
+			if char == 'S' {
+				startpoint = Coord{x, y}
+			} else if char == 'E' {
+				endpoint = Coord{x, y}
+			}
+		}
+	}
+	return
+}
+
+func calcCosts(labyrinth []string) map[Coord]int {
+	start, end := getStartEnd(labyrinth)
+
+	costs := make(map[Coord]int)
+	costs[start] = 0
+
+	curr := start
+
+	for {
+		// Check if we reached the end
+		if curr == end {
+			break
+		}
+
+		for _, dir := range []Coord{{0, 1}, {0, -1}, {1, 0}, {-1, 0}} {
+			next := Coord{curr.x + dir.x, curr.y + dir.y}
+			if labyrinth[next.y][next.x] == '#' {
+				continue
+			}
+
+			_, ok := costs[next]
+			if ok {
+				continue
+			}
+			costs[next] = costs[curr] + 1
+			curr = next
+		}
+	}
+
+	return costs
+}
+
+func checkCheats(labyrinth []string, costs map[Coord]int, minSave int) int {
+	cheats := 0
+	costsMap := map[int]int{}
+
+	for y, line := range labyrinth {
+		for x, char := range line {
+			if char != '#' {
+				cheatCosts := checkCheatsFromPoint(costs, Coord{x, y})
+				for save, count := range cheatCosts {
+					costsMap[save] += count
+				}
+			}
+		}
+	}
+
+	saves := make([]int, len(costsMap))
+	i := 0
+	for k := range costsMap {
+		saves[i] = k
+		i++
+	}
+	slices.Sort(saves)
+
+	for _, save := range saves {
+		count := costsMap[save]
+		// fmt.Printf("There are %d cheats that save %d picoseconds\n", count, save)
+		if save >= minSave {
+			cheats += count
+		}
+	}
+
+	return cheats
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func checkCheatsFromPoint(costs map[Coord]int, point Coord) map[int]int {
+	saveMap := map[int]int{}
+
+	for dx := -2; dx <= 2; dx++ {
+		for dy := -2; dy <= 2; dy++ {
+			shortcutCost := abs(dx) + abs(dy)
+
+			if shortcutCost != 2 {
+				continue
+			}
+
+			neighbour := Coord{point.x + dx, point.y + dy}
+
+			_, ok := costs[neighbour]
+			if !ok {
+				continue
+			}
+
+			if costs[neighbour] > costs[point]+shortcutCost {
+				// fmt.Printf("Cheat found at %d,%d to %d, %d with cost saving %d\n", point.x, point.y, neighbour.x, neighbour.y, costs[neighbour]-costs[point]-shortcutCost)
+				saveMap[costs[neighbour]-costs[point]-shortcutCost]++
+			}
+		}
+	}
+	return saveMap
+}
+
+func printLabyrinthWithCosts(labyrinth []string, costs map[Coord]int) {
+	maxCost := 0
+	for _, cost := range costs {
+		if cost > maxCost {
+			maxCost = cost
+		}
+	}
+
+	fmt.Print("   ")
+	for x := 0; x < len(labyrinth[0]); x++ {
+		fmt.Printf("%d", x%10)
+	}
+	fmt.Println()
+
+	for y, line := range labyrinth {
+		fmt.Printf("%2d ", y)
+		for x, char := range line {
+			if char == '#' {
+				if _, ok := costs[Coord{x, y}]; ok {
+					panic("Costs should not be calculated for walls")
+				}
+				ColorPrint("\033[30m", "█")
+			} else {
+				cost, ok := costs[Coord{x, y}]
+				if ok {
+					r, g, b := HSLtoRGB(float64(cost)/float64(maxCost)*360, 1, 0.5)
+					ColorPrint(RGBtoAnsiEscapeString(r, g, b, true), "█")
+				} else {
+					fmt.Print(" ") // should never happen
+				}
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func main() {
+	// Open the file
+	file, err := os.Open("20.in")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file line by line
+	scanner := bufio.NewScanner(file)
+
+	labyrinth := []string{}
+	// Iterate through each line
+	for scanner.Scan() {
+		line := scanner.Text()
+		labyrinth = append(labyrinth, line)
+	}
+
+	costs := calcCosts(labyrinth)
+	// printLabyrinthWithCosts(labyrinth, costs)
+
+	var sum = 0
+	const minSave = 100
+	sum = checkCheats(labyrinth, costs, minSave)
+
+	// Check for errors during scanning
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
---
+++ 20/colorLib.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+
+const (
+	UNICODE_BLOCK                         = "█"
+	UNICODE_CIRCLE                        = "⏺"
+	UNICODE_MIDDLE_DOT                    = "·"
+	UNICODE_WHITE_CIRCLE                  = "○"
+	UNICODE_BLACK_CIRCLE                  = "●"
+	UNICODE_WHITE_SQUARE                  = "□"
+	UNICODE_BLACK_SQUARE                  = "■"
+	UNICODE_WHITE_TRIANGLE                = "△"
+	UNICODE_BLACK_TRIANGLE                = "▲"
+	UNICODE_WHITE_UP_POINTING_TRIANGLE    = "▵"
+	UNICODE_BLACK_UP_POINTING_TRIANGLE    = "▴"
+	UNICODE_WHITE_DOWN_POINTING_TRIANGLE  = "▿"
+	UNICODE_BLACK_DOWN_POINTING_TRIANGLE  = "▾"
+	UNICODE_WHITE_LEFT_POINTING_TRIANGLE  = "◁"
+	UNICODE_BLACK_LEFT_POINTING_TRIANGLE  = "◂"
+	UNICODE_WHITE_RIGHT_POINTING_TRIANGLE = "▷"
+	UNICODE_BLACK_RIGHT_POINTING_TRIANGLE = "▸"
+	UNICODE_WHITE_DIAMOND                 = "◇"
+	UNICODE_BLACK_DIAMOND                 = "◆"
+	UNICODE_WHITE_LOZENGE                 = "◊"
+	UNICODE_BLACK_LOZENGE                 = "◈"
+)
+
+const ANSI_ESCAPE = "\033["
+const ANSI_END = "m"
+
+const ANSI_RESET = "\033[0m"
+
+const (
+	ANSI_BOLD              = 1
+	ANSI_ITALIC            = 3
+	ANSI_UNDERLINE         = 4
+	ANSI_SLOW_BLINK        = 5
+	ANSI_FAST_BLINK        = 6
+	ANSI_REVERSE_VIDEO     = 7
+	ANSI_CROSSED_OUT       = 9
+	ANSI_DOUBLE_UNDERLINE  = 21
+	ANSI_NOT_ITALIC        = 23
+	ANSI_NOT_UNDERLINED    = 24
+	ANSI_NOT_BLINKING      = 25
+	ANSI_NOT_REVERSE_VIDEO = 27
+	ANSI_NOT_CROSSED_OUT   = 29
+
+	ANSI_BLACK_FG   = 30
+	ANSI_RED_FG     = 31
+	ANSI_GREEN_FG   = 32
+	ANSI_YELLOW_FG  = 33
+	ANSI_BLUE_FG    = 34
+	ANSI_MAGENTA_FG = 35
+	ANSI_CYAN_FG    = 36
+	ANSI_WHITE_FG   = 37
+
+	ANSI_BLACK_BG   = 40
+	ANSI_RED_BG     = 41
+	ANSI_GREEN_BG   = 42
+	ANSI_YELLOW_BG  = 43
+	ANSI_BLUE_BG    = 44
+	ANSI_MAGENTA_BG = 45
+	ANSI_CYAN_BG    = 46
+	ANSI_WHITE_BG   = 47
+
+	ANSI_SET_FOREGROUND_COLOR   = 38
+	ANSI_RESET_FOREGROUND_COLOR = 39
+	ANSI_SET_BACKGROUD_COLOR    = 48
+	ANSI_RESET_BACKGROUND_COLOR = 49
+	ANSI_OVERLINED              = 53
+	ANSI_NOT_OVERLINED          = 55
+	ANSI_SET_OVERLINE_COLOR     = 58
+	ANSI_RESET_UNDERLINE_COLOR  = 59
+
+	ANSI_BRIGHT_BLACK_FG   = 90
+	ANSI_BRIGHT_RED_FG     = 91
+	ANSI_BRIGHT_GREEN_FG   = 92
+	ANSI_BRIGHT_YELLOW_FG  = 93
+	ANSI_BRIGHT_BLUE_FG    = 94
+	ANSI_BRIGHT_MAGENTA_FG = 95
+	ANSI_BRIGHT_CYAN_FG    = 96
+	ANSI_BRIGHT_WHITE_FG   = 97
+
+	ANSI_BRIGHT_BLACK_BG   = 100
+	ANSI_BRIGHT_RED_BG     = 101
+	ANSI_BRIGHT_GREEN_BG   = 102
+	ANSI_BRIGHT_YELLOW_BG  = 103
+	ANSI_BRIGHT_BLUE_BG    = 104
+	ANSI_BRIGHT_MAGENTA_BG = 105
+	ANSI_BRIGHT_CYAN_BG    = 106
+	ANSI_BRIGHT_WHITE_BG   = 107
+)
+
+func RGBtoAnsiEscapeString(r, g, b uint8, fg bool) string {
+	if fg {
+		return fmt.Sprintf("\033[%d;2;%d;%d;%dm", ANSI_SET_FOREGROUND_COLOR, r, g, b)
+	} else {
+		return fmt.Sprintf("\033[%d;2;%d;%d;%dm", ANSI_SET_BACKGROUD_COLOR, r, g, b)
+	}
+}
+
+func Color8BitToAnsiEscapeString(color uint8, fg bool) string {
+	if fg {
+		return fmt.Sprintf("\033[%d;5;%dm", ANSI_SET_FOREGROUND_COLOR, color)
+	} else {
+		return fmt.Sprintf("\033[%d;5;%dm", ANSI_SET_BACKGROUD_COLOR, color)
+	}
+}
+
+func ConstantToAnsiEscapeString(constant uint8) string {
+	return fmt.Sprintf("%s%d%s", ANSI_ESCAPE, constant, ANSI_END)
+}
+
+func HSLtoRGB(h, s, l float64) (r, g, b uint8) {
+	if s == 0 {
+		// Achromatic color (gray scale)
+		v := uint8(math.Round(l * 255))
+		return v, v, v
+	}
+
+	if s > 1 || s < 0 {
+		panic("Saturation must be between 0 and 1")
+	}
+
+	if l > 1 || l < 0 {
+		panic("Lightness must be between 0 and 1")
+	}
+
+	h = math.Mod(h, 360)
+
+	var hueToRGB = func(p, q, t float64) float64 {
+		if t < 0 {
+			t += 1
+		}
+		if t > 1 {
+			t -= 1
+		}
+		if t < 1/6.0 {
+			return p + (q-p)*6*t
+		}
+		if t < 1/2.0 {
+			return q
+		}
+		if t < 2/3.0 {
+			return p + (q-p)*(2/3.0-t)*6
+		}
+		return p
+	}
+
+	q := l * (1 + s)
+	if l >= 0.5 {
+		q = l + s - l*s
+	}
+	p := 2*l - q
+
+	h /= 360 // Normalize hue to range [0,1]
+	rFloat := hueToRGB(p, q, h+1/3.0)
+	gFloat := hueToRGB(p, q, h)
+	bFloat := hueToRGB(p, q, h-1/3.0)
+
+	r = uint8(int(math.Round(rFloat * 255)))
+	g = uint8(int(math.Round(gFloat * 255)))
+	b = uint8(int(math.Round(bFloat * 255)))
+	return
+}
+
+type TerminalColor struct {
+	r, g, b uint8
+	fg      bool
+}
+
+func NewTerminalColor(r, g, b uint8, fg bool) TerminalColor {
+	return TerminalColor{r, g, b, fg}
+}
+
+func (c TerminalColor) ToAnsiEscapeString() string {
+	return RGBtoAnsiEscapeString(c.r, c.g, c.b, c.fg)
+}
+
+func ColorPrintf(format string, ansi string, a ...interface{}) {
+	fmt.Printf("%s%s%s", ansi, fmt.Sprintf(format, a...), ANSI_RESET)
+}
+
+func ColorPrintln(ansi string, a ...interface{}) {
+	fmt.Print(ansi)
+	fmt.Println(a...)
+	fmt.Print(ANSI_RESET)
+}
+
+func ColorPrint(ansi string, a ...interface{}) {
+	fmt.Print(ansi)
+	fmt.Print(a...)
+	fmt.Print(ANSI_RESET)
+}
