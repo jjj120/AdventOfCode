0ead8afb0f9ca430f3d92a4b835f6f4ee860fa39
Jonas Mueller <66848337+jjj120@users.noreply.github.com>
2025-12-03 07:59:19 +0100

    Add Dec03 ex2

--- 03/03.go
+++ 03/03.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
@@ -11,37 +12,61 @@ import (
 const day = 03
 const selectExample = false
 
-func getMaxJoltage(batteriesStr string) int {
-	batteriesSplit := strings.Split(batteriesStr, "")
-	batteries := make([]int, 0, len(batteriesStr))
-
-	maxBat := -1
+func maxInSlice(s []int) (int, int) {
+	if len(s) <= 0 {
+		fmt.Printf("%v\n", s)
+		panic("Slice is empty!")
+	}
 	maxInd := 0
-	for i, bat := range batteriesSplit {
-		batInt, err := strconv.Atoi(bat)
-		aoc.Check(err)
-		batteries = append(batteries, batInt)
+	maxVal := s[0]
 
-		if batInt > maxBat && i != len(batteriesStr)-1 {
-			// dont use the last entry
-			maxBat = batInt
+	for i, v := range s {
+		if v > maxVal {
+			maxVal = v
 			maxInd = i
 		}
 	}
+	return maxVal, maxInd
+}
 
-	secondMax := 0
-	for i := maxInd + 1; i < len(batteries); i++ {
-		secondMax = max(batteries[i], secondMax)
+func getMaxJoltage(batteriesStr string, numTurnOn int) int {
+	batteriesSplit := strings.Split(batteriesStr, "")
+	batteries := make([]int, 0, len(batteriesStr))
+
+	for _, bat := range batteriesSplit {
+		batInt, err := strconv.Atoi(bat)
+		aoc.Check(err)
+		batteries = append(batteries, batInt)
 	}
-	fmt.Printf("Got max1 %d at %d and max2 %d in %v\n", maxBat, maxInd, secondMax, batteries)
 
-	return maxBat*10 + secondMax
+	maxVal, maxInd := maxInSlice(batteries[0 : len(batteries)-numTurnOn+1])
+	fmt.Printf("Got maxVal %d at %d in %v for index %d\n", maxVal, maxInd, batteries, 0)
+
+	maxValues := make([]int, numTurnOn, numTurnOn)
+	maxValues[0] = maxVal
+
+	for maxValueIndex := 1; maxValueIndex < numTurnOn; maxValueIndex++ {
+		maxVal, newMaxInd := maxInSlice(batteries[maxInd+1 : len(batteries)-numTurnOn+maxValueIndex+1])
+		maxInd += newMaxInd + 1
+
+		fmt.Printf("Got maxVal %d at %d in %v for index %d\n", maxVal, maxInd, batteries, maxValueIndex)
+		maxValues[maxValueIndex] = maxVal
+	}
+
+	maxValue := 0
+	for i, v := range maxValues {
+		maxValue += int(math.Pow(10., float64(numTurnOn-i-1))) * v
+	}
+
+	fmt.Printf("Got max %d in %v\n", maxValue, batteries)
+
+	return maxValue
 }
 
 func handleLines(lines []string) int {
 	sum := 0
 	for _, line := range lines {
-		sum += getMaxJoltage(line)
+		sum += getMaxJoltage(line, 12)
 	}
 	return sum
 }
@@ -54,6 +79,6 @@ func main() {
 
 	fmt.Printf("Sum: %d\n", sum)
 	if selectExample {
-		aoc.Assert(sum == 357, "Example is wrong")
+		aoc.Assert(sum == 3121910778619, "Example is wrong")
 	}
 }
