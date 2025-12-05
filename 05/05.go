d3cb6de81704bcb5b1425c503bbe133d2377e9ac
Jonas Mueller <66848337+jjj120@users.noreply.github.com>
2025-12-05 07:57:01 +0100

    Add Dec05 ex2

--- 05/05.go
+++ 05/05.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 
@@ -11,12 +12,83 @@ const day = 05
 const selectExample = false
 
 type dataRange struct {
-	low  int
-	high int
+	low   int
+	high  int
+	empty bool
+}
+
+func (d *dataRange) width() int {
+	if !d.empty {
+		return d.high - d.low + 1
+	}
+	return 0
 }
 
 func (d *dataRange) includes(v int) bool {
-	return v >= d.low && v <= d.high
+	return v >= d.low && v <= d.high && !d.empty
+}
+
+func (d *dataRange) overlaps(nd dataRange) bool {
+	return (d.includes(nd.low) || d.includes(nd.high)) && !d.empty && !nd.empty
+}
+
+func (d *dataRange) mergeInPlace(nd dataRange) error {
+	if !d.overlaps(nd) {
+		return errors.New("Merge of non overlapping ranges")
+	}
+	if nd.empty {
+		return nil
+	}
+	if d.empty {
+		d.empty = false
+		d.low = nd.low
+		d.high = nd.high
+		return nil
+	}
+
+	d.low = min(d.low, nd.low)
+	d.high = max(d.high, nd.high)
+	return nil
+}
+
+func hasOverlaps(ranges []dataRange) bool {
+	for i, r1 := range ranges {
+		for j, r2 := range ranges {
+			// merge ranges together
+			if i != j && r1.overlaps(r2) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func countFreshRange(ranges []dataRange, ingredients []int) int {
+	for hasOverlaps(ranges) {
+		for i, r1 := range ranges {
+			for j, r2 := range ranges {
+				// merge ranges together
+				if i != j && r1.overlaps(r2) {
+					ranges[i].mergeInPlace(r2)
+					ranges[j].empty = true
+				}
+			}
+		}
+	}
+
+	aoc.Assert(!hasOverlaps(ranges), "Still detected overlaps!")
+
+	// sum up ranges
+	sum := 0
+	nonempty := 0
+	for _, r := range ranges {
+		sum += r.width()
+		if !r.empty {
+			nonempty++
+		}
+	}
+	fmt.Printf("Found %d non-overlapping ranges\n", nonempty)
+	return sum
 }
 
 func countFresh(ranges []dataRange, ingredients []int) int {
@@ -40,11 +112,11 @@ func handleLines(lines []string) int {
 	for _, line := range lines {
 		if len(line) == 0 {
 			isRanges = false
-			continue
+			break
 		}
 
 		if isRanges {
-			newRange := dataRange{0, 0}
+			newRange := dataRange{0, 0, false}
 			_, err := fmt.Sscanf(line, "%d-%d", &newRange.low, &newRange.high)
 			aoc.Check(err)
 			ranges = append(ranges, newRange)
@@ -55,10 +127,10 @@ func handleLines(lines []string) int {
 		}
 	}
 
-	fmt.Printf("Ranges: %v\n", ranges)
-	fmt.Printf("Ingredients: %v\n", ingredients)
+	// fmt.Printf("Ranges: %v\n", ranges)
+	// fmt.Printf("Ingredients: %v\n", ingredients)
 
-	return countFresh(ranges, ingredients)
+	return countFreshRange(ranges, ingredients)
 }
 
 func main() {
@@ -69,6 +141,8 @@ func main() {
 
 	fmt.Printf("Sum: %d\n", sum)
 	if selectExample {
-		aoc.Assert(sum == 3, "Example solution is wrong")
+		aoc.Assert(sum == 14, "Example solution is wrong")
+	} else {
+		aoc.Assert(sum <= 353600416748441, "Real solution is too high")
 	}
 }
