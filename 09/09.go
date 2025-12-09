b484e6fa2239af5e035a7e3532a904b2a5087687
Jonas Mueller <66848337+jjj120@users.noreply.github.com>
2025-12-09 11:32:58 +0100

    Add Dec09 ex2 image plot function

--- 09/09.go
+++ 09/09.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"os"
 
 	aoc "github.com/jjj120/AdventOfCode/lib"
 )
@@ -17,7 +22,30 @@ const (
 	colGreen
 )
 
-func printTileMap(tiles map[aoc.Vec2d]Color, width int, height int) {
+type TileColorMap map[aoc.Vec2d]Color
+
+func (m TileColorMap) Inside(p aoc.Vec2d) bool {
+	inside := false
+	c, ok := m[p]
+	y := p.Y
+
+	for x := range p.X {
+		pos := aoc.Vec2d{X: x, Y: y}
+		_, exists := m[pos]
+		_, exists_next := m[pos.Add(aoc.Vec2d{X: 1, Y: 0})]
+
+		if exists && !exists_next {
+			inside = !inside
+		}
+	}
+	return inside || (ok && c != colWhite)
+}
+
+func (m TileColorMap) Outside(p aoc.Vec2d) bool {
+	return !m.Inside(p)
+}
+
+func printTileMap(tiles TileColorMap, width int, height int) {
 	if !selectExample {
 		return
 	}
@@ -42,11 +70,56 @@ func printTileMap(tiles map[aoc.Vec2d]Color, width int, height int) {
 	}
 }
 
-func makeTileMap(tiles []aoc.Vec2d, width, height int) map[aoc.Vec2d]Color {
-	fmt.Printf("Start making tilemap with size %dx%d = %d\n", width, height, width*height)
-	// 2147483647 is max
-	// 9672230450
-	colorMap := make(map[aoc.Vec2d]Color, width*height/16)
+func drawTileMap(tiles TileColorMap, width, height int) {
+	fmt.Println("Start drawing tilemap                                  ")
+	const downscaleFactor = 64
+	baseImage := image.NewRGBA(image.Rect(0, 0, width/downscaleFactor+1, height/downscaleFactor+1))
+
+	for y := range height / downscaleFactor {
+		fmt.Printf("Drawing line %d/%d         \r", y, height/downscaleFactor)
+		for x := range width / downscaleFactor {
+
+			hasRed := false
+			hasGreen := false
+			for xOff := range downscaleFactor {
+				for yOff := range downscaleFactor {
+					pos := aoc.Vec2d{X: x*downscaleFactor + xOff, Y: y*downscaleFactor + yOff}
+
+					v, ok := tiles[pos]
+					if ok && v == colRed {
+						hasRed = true
+					}
+					if ok && v == colGreen {
+						hasGreen = true
+					}
+				}
+			}
+			if hasRed {
+				baseImage.SetRGBA(x, y, color.RGBA{R: 255, G: 0, B: 0, A: 255})
+			} else if hasGreen {
+				baseImage.SetRGBA(x, y, color.RGBA{R: 0, G: 255, B: 0, A: 255})
+			} else {
+				baseImage.Set(x, y, color.Black)
+			}
+		}
+	}
+
+	file, err := os.Create("output.png")
+	if err != nil {
+		log.Fatalf("Error creating file: %v", err)
+	}
+	defer file.Close()
+
+	if err := png.Encode(file, baseImage); err != nil {
+		log.Fatalf("Error encoding image: %v", err)
+	}
+	fmt.Println("Finished drawing tilemap")
+}
+
+func makeTileMap(tiles []aoc.Vec2d, width, height int) TileColorMap {
+	fmt.Printf("Start making tilemap")
+
+	colorMap := make(TileColorMap)
 	prevTile := tiles[0]
 
 	// color edges
@@ -76,33 +149,12 @@ func makeTileMap(tiles []aoc.Vec2d, width, height int) map[aoc.Vec2d]Color {
 		colorMap[prevTile] = colGreen
 	}
 	colorMap[tiles[0]] = colRed
-	fmt.Println("Start coloring inside                ")
-
-	// color inside of all
-	for y := range height {
-		fmt.Printf("Currently at line %d/%d                                             \r", y, height)
-		inside := false
-
-		for x := range width {
-			pos := aoc.Vec2d{X: x, Y: y}
-			_, exists := colorMap[pos]
-			_, exists_next := colorMap[pos.Add(aoc.Vec2d{X: 1, Y: 0})]
-
-			if inside && !exists {
-				colorMap[pos] = colGreen
-			}
-
-			if exists && !exists_next {
-				inside = !inside
-			}
-		}
-	}
 
 	return colorMap
 }
 
-func getBiggestRect(tiles map[aoc.Vec2d]Color) int {
-	fmt.Println("Start checking rects")
+func getBiggestRect(tiles TileColorMap) int {
+	fmt.Println("Start checking rects                             ")
 
 	maxSize := 0
 	maxSizeP1 := aoc.Vec2d{X: 0, Y: 0}
@@ -121,9 +173,11 @@ func getBiggestRect(tiles map[aoc.Vec2d]Color) int {
 			fmt.Printf("Checking %v-%v              \r", p1, p2)
 
 			isValid := true
-			for x := min(p1.X, p2.X); x <= max(p1.X, p2.X) && isValid; x++ {
-				for y := min(p1.Y, p2.Y); y <= max(p1.Y, p2.Y); y++ {
-					if c, ok := tiles[aoc.Vec2d{X: x, Y: y}]; !ok || c == colWhite {
+			for y := min(p1.Y, p2.Y); y <= max(p1.Y, p2.Y) && isValid; y++ {
+				for x := min(p1.X, p2.X); x <= max(p1.X, p2.X) && isValid; x++ {
+					fmt.Printf("Checking %v-%v at %d,%d                        \r", p1, p2, x, y)
+
+					if tiles.Outside(aoc.Vec2d{X: x, Y: y}) {
 						isValid = false
 						break
 					}
@@ -134,7 +188,7 @@ func getBiggestRect(tiles map[aoc.Vec2d]Color) int {
 			}
 
 			size := (max(p1.X, p2.X) - min(p1.X, p2.X) + 1) * (max(p1.Y, p2.Y) - min(p1.Y, p2.Y) + 1)
-
+			fmt.Printf("Got size %d at %v-%v                                     \r", size, p1, p2)
 			if size > maxSize {
 				maxSizeP1 = p1
 				maxSizeP2 = p2
@@ -153,6 +207,8 @@ func handleLines(lines []string) int {
 	tiles := make([]aoc.Vec2d, 0)
 	maxX := 0
 	maxY := 0
+	minX := 1 << 31
+	minY := 1 << 31
 	for _, line := range lines {
 		var tile aoc.Vec2d
 		fmt.Sscanf(line, "%d,%d", &tile.X, &tile.Y)
@@ -160,9 +216,15 @@ func handleLines(lines []string) int {
 
 		maxX = max(maxX, tile.X)
 		maxY = max(maxY, tile.Y)
+		minX = min(minX, tile.X)
+		minY = min(minY, tile.Y)
 	}
+	fmt.Printf("X: min: %d, max: %d\n", minX, maxX)
+	fmt.Printf("Y: min: %d, max: %d\n", minY, maxY)
 	tileMap := makeTileMap(tiles, maxX+1, maxY+1)
 	printTileMap(tileMap, maxX+2, maxY+2)
+	drawTileMap(tileMap, maxX+2, maxY+2)
+
 	return getBiggestRect(tileMap)
 }
 
