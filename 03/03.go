6647c20efa15290470d5eb33c9b8b35b33b81f57
Jonas Mueller <jjj120.git@gmail.com>
2025-12-26 20:25:41 +0100

    Add Dec03 ex1

---
+++ 03/03.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	aoc "github.com/jjj120/AdventOfCode/lib"
+)
+
+const day = 03
+const selectExample = false
+
+func checkTriangle(l1, l2, l3 int) bool {
+	return (l1+l2 > l3 && l2+l3 > l1 && l3+l1 > l2)
+}
+
+func handleLines(lines []string) int {
+	counter := 0
+	for _, line := range lines {
+		s := strings.Split(line, " ")
+		nums := make([]int, 0, 3)
+		for i, n := range s {
+			s[i] = strings.TrimSpace(n)
+			if len(s[i]) > 0 {
+				num, err := strconv.Atoi(s[i])
+				aoc.Check(err)
+				nums = append(nums, num)
+			}
+		}
+		// fmt.Println(nums)
+		if checkTriangle(nums[0], nums[1], nums[2]) {
+			counter += 1
+		}
+	}
+	return counter
+}
+
+func main() {
+	lines, err := aoc.ParseInput(day, selectExample)
+	aoc.Check(err)
+
+	sum := handleLines(lines)
+
+	fmt.Printf("Sum: %d\n", sum)
+	if selectExample {
+		aoc.Assert(sum == 1, "Example wrong!")
+	}
+}
