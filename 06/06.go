d98af7b78950c8f82600269fc6cd0f63408161ea
Jonas Mueller <jjj120.git@gmail.com>
2025-12-27 16:33:28 +0100

    Add Dec06 ex1

---
+++ 06/06.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+
+	aoc "github.com/jjj120/AdventOfCode/lib"
+)
+
+const day = 06
+const selectExample = false
+
+func handleLines(lines []string) string {
+	maps := make([]map[rune]int, len(lines[0]))
+	for i := range maps {
+		maps[i] = make(map[rune]int)
+		for r := 'a'; r <= 'z'; r++ {
+			maps[i][r] = 0
+		}
+	}
+
+	for _, line := range lines {
+		for i, r := range line {
+			maps[i][r] += 1
+		}
+	}
+
+	sol := ""
+	for _, m := range maps {
+		maxChar := 'a'
+		maxNum := 0
+		for k, v := range m {
+			if maxNum < v {
+				maxNum = v
+				maxChar = k
+			}
+		}
+		sol += string(maxChar)
+	}
+
+	return sol
+}
+
+func main() {
+	lines, err := aoc.ParseInput(day, selectExample)
+	aoc.Check(err)
+
+	sum := handleLines(lines)
+
+	fmt.Printf("Sum: %s\n", sum)
+	if selectExample {
+		aoc.Assert(sum == "easter", "Example wrong!")
+	}
+}
